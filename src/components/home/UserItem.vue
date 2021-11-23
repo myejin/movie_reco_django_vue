cf719b1e074eb7d37998871a50e0fd1071a52ce6
김혜진 <rlagpwlsq78@gmail.com>
2021-11-23 18:16:04 +0900

    유사한 패턴의 유저 추천

---
+++ src/components/home/UserItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h3>{{ username }}님이 본 영화</h3>
+    <div>
+      <v-row>
+        <v-card
+          v-for="movie of rateMovies"
+          :key="movie.movie_id"
+          width="300"
+          style="margin: 1rem;"
+        >
+          <router-link :to="{name: 'MovieDetail', params: { movieId: movie.movie_id }}">
+            <v-img
+              class="white--text align-end"
+              :src="'https://image.tmdb.org/t/p/original/' + movie.movie_detail.poster_path"
+              width="300"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            >
+            </v-img>
+            <v-card-title style="padding: 0;">
+              <v-spacer />
+              <div class="cardTitle">{{ movie.movie_detail.title }}</div>
+              <v-spacer />
+            </v-card-title>
+          </router-link>
+        </v-card>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserItem',
+  props: {
+    user: Object,
+  },
+  data: function () {
+    return {
+      username: '',
+      rateMovies: [],
+    }
+  },
+  computed: {
+    movieDetail: function (movieId) {
+
+      return movieId
+    }
+  },
+  created: function () {
+    this.username = this.user.username
+    this.rateMovies = this.user.rate_movies
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,22 +5,24 @@
      <img width="100" class="img-size" src="@/assets/Logo.png" alt="">
     </router-link>
     <router-link :to="{ name: 'MovieDetail', params: { movieId: 1} }">detail</router-link>
-
     <weather-movie
     ></weather-movie>
     <weather></weather>
+    <similar-user></similar-user>
 </div>
 </template>
 
 <script>
 import WeatherMovie from '../components/home/WeatherMovie.vue'
 import Weather from '../components/home/Weather.vue'
+import SimilarUser from '../components/home/SimilarUser.vue'
 
 export default {
   name: 'Home',
   components:{
     WeatherMovie,
-    Weather
+    Weather,
+    SimilarUser,
   },
 }
 </script>
