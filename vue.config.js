0d799d3dc9c6b558ca29296d04759c7fb78fcdaa
김혜진 <rlagpwlsq78@gmail.com>
2021-11-23 20:57:20 +0900

    영화 디테일 페이지 기능 삽입

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-
+  devServer: {
+    host: '0.0.0.0',
+    https: false,
+    port: 8080,
+    public: 'http://0.0.0.0:8080'
+  },
 }
